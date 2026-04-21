4bde5c63640d6cd7ada068fe1705d72cdaac3bae
gouthamk16 <gouthamrakesh@icloud.com>
2026-04-21 15:17:13 +0000

    time embedding

--- ddpm.py
+++ ddpm.py
@@ -1,9 +1,13 @@
-## Linear noise scheduler
+## WIP
 
 import jax
 import jax.numpy as jnp
+import flax.linen as nn
+from typing import Any
 
 jax.config.update("jax_default_device", jax.devices("gpu")[0])
+
+## Linear noise scheduler
 
 class LinearNoiseScheduler:
     def __init__(self, num_timesteps, beta_start, beta_end):
@@ -50,3 +54,57 @@ class LinearNoiseScheduler:
             return mean + sigma * z, x0, key
         
     
+# Time embedding
+def get_time_embedding(time_steps, temb_dim):
+    assert temb_dim % 2 == 0, "time embedding dimension must be divisible by 2"
+    
+    # factor = 10000^(2i/d_model)
+    factor = 10000 ** ((jnp.arange(
+        start=0, stop=temb_dim // 2, dtype=jnp.float32) / (temb_dim // 2))
+    )
+    # pos / factor
+    # timesteps B -> B, 1 -> B, temb_dim
+    t_emb = time_steps[:, None].repeat(1, temb_dim // 2) / factor
+    t_emb = jnp.concat([jnp.sin(t_emb), jnp.cos(t_emb)], axis=-1)
+    return t_emb
+
+## The downsampling block
+class DownBlock(nn.Module):
+    num_heads: Any
+    num_layers: Any
+    down_sample: Any
+    in_channels: Any
+    out_channels: Any
+    t_embed_dim: Any
+
+    def setup(self):
+        self.resnet_conv_first = [
+            nn.Sequential([
+                nn.GroupNorm(num_groups=8),
+                nn.activation.silu,
+                nn.Conv(
+                    features = self.out_channels,
+                    kernel_size = (3, 3),
+                    strides = (1, 1),
+                    padding = 1,
+                ),  
+            ])
+            for _ in range(self.num_layers)
+        ]
+        self.attention_norms = [
+            [nn.GroupNorm(num_groups=8)] for _ in range(self.num_layers)
+        ] 
+        self.attentions = [
+            [nn.MultiHeadAttention(
+                num_heads = self.num_heads,
+            )]
+        ]
+
+    @nn.compact
+    def __call__(self, x, train: bool = False):
+        for feat in self.features[:-1]:
+            x = nn.Dense(feat)(x)
+            x = nn.relu(x)
+            x = nn.Dropout(0.3, deterministic=not train)(x)
+        x = nn.Dense(self.features[-1])(x)
+        return x
